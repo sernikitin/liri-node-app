58391ef7dfc9549c5c97e084b409c3cd543ad367
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-04-02 21:55:48 -0500

    as

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
-var keys = require('./keys.js')
-var spotify = require('spotify')
-var twitter =require('twitter')
-console.log(keys)
+var keys = require('./keys.js');
+
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
+console.log(spotify)
